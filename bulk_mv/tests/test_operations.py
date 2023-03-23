27d1ccc9c99070d947f2118a2b11dad101e834f8
Angel Garcia <ag4219@columbia.edu>
2023-03-22 22:05:47 -0400

    added test for operations and setup config

--- bulk_mv/tests/test_operations.py
+++ bulk_mv/tests/test_operations.py
@@ -1,22 +1,51 @@
 import unittest
-from bulk_mv import perform_adds, perform_deletes, perform_renames, perform_moves
+from bulk_mv import (
+    build_from_directory,
+    grammar,
+    FileTreeVisitor,
+    perform_adds,
+    perform_deletes,
+    perform_renames,
+    perform_moves,
+)
+from os import path, remove, rename
+from shutil import rmtree, move
 
 
 class TestOperations(unittest.TestCase):
     def setUp(self):
-        pass
+        with open("bulk_mv/tests/dummy_bmv_files/sample3.bmv", "r") as file:
+            tree = grammar.parse(file.read().strip())
+            ftv = FileTreeVisitor()
+            self.output = ftv.visit(tree)
 
     def test_perform_adds(self):
-        pass
+        perform_adds(self.output["add"])
+
+        self.assertTrue(path.exists("./bulk_mv/tests/dummy_directories/sample3/new_file.txt"))
+
+        remove("./bulk_mv/tests/dummy_directories/sample3/new_file.txt")
 
     def test_perform_deletes(self):
-        pass
+        perform_deletes(self.output["delete"])
+
+        self.assertTrue(not path.exists("./bulk_mv/tests/dummy_directories/sample3/markdown/2.md"))
+
+        open("./bulk_mv/tests/dummy_directories/sample3/markdown/2.md", 'a').close()
 
     def test_perform_renames(self):
-        pass
+        perform_renames(self.output["rename"])
+
+        self.assertTrue(path.exists("./bulk_mv/tests/dummy_directories/sample3/txt/"))
+
+        rename("./bulk_mv/tests/dummy_directories/sample3/txt/", "./bulk_mv/tests/dummy_directories/sample3/text/")
 
     def test_perform_moves(self):
-        pass
+        perform_moves(self.output["move"])
 
-    def tearDown(self):
-        pass
+        self.assertTrue(path.exists("./bulk_mv/tests/dummy_directories/sample3/photos/markdown/"))
+
+        move(
+            "./bulk_mv/tests/dummy_directories/sample3/photos/markdown/",
+            "./bulk_mv/tests/dummy_directories/sample3//markdown",
+        )
