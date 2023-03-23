194ef622e4b46f0859f46e199663b0a2caa31821
Angel Garcia <ag4219@columbia.edu>
2023-03-23 00:10:23 -0400

    Bump version: 0.1.1 → 0.1.2

--- bulk_mv/__init__.py
+++ bulk_mv/__init__.py
@@ -4,4 +4,4 @@ from .bmv_parser import grammar, FileTreeVisitor
 from .helpers import get_last_index_of_substring
 from .operations import perform_adds, perform_deletes, perform_renames, perform_moves
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
