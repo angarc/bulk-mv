ed0be1eb8ec6dc7d9f3db1d59749d6fadaac03b8
Angel Garcia <ag4219@columbia.edu>
2023-03-22 22:26:14 -0400

    Fixed setup.py bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='bulk-mv',
-    version='0.1.0',
+    version = "0.1.0",
     description='Move, rename, delete, and add files as fast as you can use Vim.',
     author='Angel Garcia',
     author_email='angarc37@gmail.com',
