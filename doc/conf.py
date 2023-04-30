27b3fbd15081a050a85aacc73590b9ce0318d00f
Angel Garcia <ag4219@columbia.edu>
2023-04-29 22:32:35 -0400

    Bump version: 0.1.4 → 0.1.5

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-version = "0.1.4"
+version = "0.1.5"
 release = version
 
 
